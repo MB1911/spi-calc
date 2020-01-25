79733744af6fad7f4646e5a795830183b7d163cc
MB1911 <buch.manan2010@gmail.com>
2020-01-25 17:15:48 +0530

    Updates

---
+++ precache-manifest.870a0ceca8c98640e87b067775bce9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "085298fbc66ada009ce8d783a1990a8c",
+    "url": "/spi-calc/index.html"
+  },
+  {
+    "revision": "bed06b95d14d641144ec",
+    "url": "/spi-calc/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5a4ebc266148c033fe32",
+    "url": "/spi-calc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bed06b95d14d641144ec",
+    "url": "/spi-calc/static/js/2.749eef7c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spi-calc/static/js/2.749eef7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a4ebc266148c033fe32",
+    "url": "/spi-calc/static/js/main.bd5cbd25.chunk.js"
+  },
+  {
+    "revision": "6abddeae178baad8382d",
+    "url": "/spi-calc/static/js/runtime-main.ddf20f28.js"
+  }
+]);
